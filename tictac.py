a5b4e8fbf98b8d4ea8c8db46fc484d091af62516
Allison Harris <allison.harris23@gmail.com>
2017-09-02 14:18:55 -0500

    Complete step 1

--- tictac.py
+++ tictac.py
@@ -1,10 +1,21 @@
 from __future__ import print_function 
 
-# Step 1: Set up your board as a list, where each index 1-9 corresponds with a number on a number pad
+# Step 1: Write a function that can print out a board. Set up your board as a list, where each index 1-9 corresponds with 
+#a number on a number pad so you get a 3 by 3 board representation.
 from IPython.display import clear_output
 def display_board(board):
-    
-    pass
+    clear_output()
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
 
 # Step 2: Write a function that can take in a player input and assign their marker as 'X' or 'O'
 def player_input():
